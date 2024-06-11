a57e49e1b5302f1985d53c8ac8cbefbbb6ccde04
Irfaan Meera.E <irfaanmeera@gmail.com>
2024-06-11 19:46:19 +0530

    Completed signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,23 +1,77 @@
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Signup() {
+export default function Signup() {
+    const [formData,setFormData] = useState({})
+    const[error,setError]= useState(null)  
+    const [loading,setLoading] = useState(false)                                                      
+    const handleChange =(e)=>{
+       setFormData({...formData, [e.target.id]:e.target.value})
+    }
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+        try{
+            setLoading(true)
+        const res = await fetch('/api/auth/signup',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(formData)
+            
+        })
+        const data = await res.json();
+        console.log(data)
+        setLoading(false)
+        if(data.success === false){
+            setError(true)
+            return;
+        }
+        setError(false)
+    }catch(err){
+      setLoading(false);
+      setError(true);
+    }
+    }
   return (
     <div className="p-3 max-w-md mx-auto ">
       <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
-      <form className='flex flex-col gap-4 '>
-        <input type="text" placeholder="Username" id="username" className='bg-slate-100 p-3 rounded-lg my-2' />
-        <input type="email" placeholder="Email" id="email" className='bg-slate-100 p-3 rounded-lg my-2' />
-        <input type="password" placeholder="Password" id="password" className='bg-slate-100 p-3 rounded-lg my-2' />
-        <button className='bg-slate-700 text-white rounded-lg uppercase hover:capacity-95 disabled:opacity-80 max-w-sm p-2 ml-5 my-5'>Sign Up</button>
+      <p className='text-red-700 mt-5 mb-5 font-semibold'>{error && 'Username already exists'}</p>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg my-2"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg my-2"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg my-2"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white rounded-lg uppercase hover:capacity-95 disabled:opacity-80 max-w-sm p-2 ml-5 my-5">
+        {loading ? 'Loading....' : 'Sign Up'}
+        </button>
       </form>
-      <div className='flex gap-2 mt-4 ml-5'>
+      <div className="flex gap-2 mt-4 ml-5">
         <p>Have an account?</p>
-         <Link to='/sign-in'>
-        <span className='text-blue-500'>Sign in</span>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+    
     </div>
-  )
+  );
 }
 
-export default Signup
+
