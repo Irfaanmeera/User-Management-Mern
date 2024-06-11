8d229f3abebf852c699474543b589b68498d9edf
Irfaan Meera.E <irfaanmeera@gmail.com>
2024-06-11 17:32:29 +0530

    Hashed password using bcrypt

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
 dotenv.config();
 
 
@@ -15,9 +16,12 @@ mongoose.connect(process.env.MONGO)
   
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000, ()=>{
     console.log('Server listening on port 3000')
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
 
